554f5e577e461afd46da25ca3415b93b757bb3de
franktranvantu <59758589+franktranvantu@users.noreply.github.com>
2021-04-27 17:09:12 +0700

    Update

--- react-bootstrap/example/src/App.js
+++ react-bootstrap/example/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import StudentList from "./student/StudentList";
 import {Container} from "react-bootstrap";
+import StudentForm from "./student/StudentForm";
 
 function App() {
   return (
     <Container>
-      <StudentList />
+      {/*<StudentList />*/}
+      <StudentForm />
     </Container>
   );
 }
--- react-bootstrap/example/src/student/StudentForm.js
+++ react-bootstrap/example/src/student/StudentForm.js
@@ -1,9 +1,40 @@
-import {Form} from "react-bootstrap";
+import {Col, Form, Row} from "react-bootstrap";
+import "react-datepicker/dist/react-datepicker.min.css";
+import DatePicker from "react-datepicker";
+import {useState} from "react";
 
 export default function StudentForm() {
+
+  const [startDate, setStartDate] = useState(new Date());
+
   return (
     <Form>
-
+      <Row>
+        <Col>
+          <Form.Group controlId="name">
+            <Form.Label>Name</Form.Label>
+            <Form.Control as="input" />
+          </Form.Group>
+        </Col>
+        <Col>
+          <Form.Group controlId="email">
+            <Form.Label>Email</Form.Label>
+            <Form.Control type="email" />
+          </Form.Group>
+        </Col>
+        <Col>
+          <Form.Group controlId="dob">
+            <Form.Label>Birthday</Form.Label>
+            <DatePicker
+              wrapperClassName="form-control"
+              className="form-control"
+              selected={startDate}
+              dateFormat="dd/MM/yyyy"
+              onChange={date => setStartDate(date)}
+            />
+          </Form.Group>
+        </Col>
+      </Row>
     </Form>
   );
 }
